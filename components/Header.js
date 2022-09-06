6bd7fb32e104512663d2d58d9dc0a3a3839a5329
Mariano <marianonunezma@gmail.com>
2022-09-06 16:41:17 -0300

    Logo Ready

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+
+import LogoImg from "../public/logo.png"
 
 import { BsCart2 } from 'react-icons/bs';
 
@@ -13,8 +16,12 @@ const Header = () => {
 
     return ( 
         <header>
-          <nav className="flex justify-between bg-slate-900 text-white text-lg px-10 py-5">
-            <Link href="/">Logo</Link>
+          <nav className="flex justify-between items-center bg-slate-900 text-white text-lg px-10 py-3">
+            <Link href="/">
+                <a className="grid">
+                    <Image src={LogoImg} alt='Logo Image' objectFit="contain" objectPosition="center" width={120} height={60} />
+                </a>
+            </Link>
             <ul className="flex gap-14">
                 <li className={route === '/categories' ? 'text-cyan-600' : 'hover:text-cyan-600'}>
                     <Link href="/categories">Categories</Link>
